0c05693ce3eaa5f34c4078d013964fe2ea12f310
mariamgamal70 <mariamgamal70@gmail.com>
2023-03-19 03:28:07 +0200

    summary
    index file updated
    style for horizontal line added
    app.js added responsiveness(needs fix ) and variables

--- website/app.js
+++ website/app.js
@@ -1,6 +1,16 @@
-let signalGraph= document.getElementById('signal');
+let signalGraph = document.getElementById("signal");
 let reconstructedGraph = document.getElementById("reconstructed");
 let differenceGraph = document.getElementById("difference");
+
+let uploadFile = document.getElementById("signaluploadfile");
+let frequency = document.getElementById("frequency");
+let amplitude = document.getElementById("amplitude");
+let signalComposerButton = document.getElementById("addsignalcomposer");
+let samplingFrequency = document.getElementById("samplingfrequency");
+let SNR = document.getElementById("snr");
+let SNRvalue = document.getElementById("noisevalue");
+let signalComponentSelect = document.getElementById("components");
+let removeSignalComponentButton = document.getElementById("removecomponent");
 
 document.onload = createPlot(signalGraph);
 document.onload = createPlot(reconstructedGraph);
@@ -10,29 +20,30 @@ function createPlot(graphElement) {
   let layout = {
     title: { title: "Click Here<br>to Edit Chart Title" },
     xaxis: {
-    //   rangeslider: {
-    //     range: [0, 1],
-    //     visible: true,
-    //     dragmode: false,
-    //     zoom: false,
-    //   },
+      //   rangeslider: {
+      //     range: [0, 1],
+      //     visible: true,
+      //     dragmode: false,
+      //     zoom: false,
+      //   },
       range: [0, 5],
-    //   rangemode: "tozero",
+      //   rangemode: "tozero",
       title: "Time (s)",
       zoom: 1000,
-    //   fixedrange: true,
+      //   fixedrange: true,
     },
     yaxis: {
       title: "Amplitude",
-    //   fixedrange: true,
+      //   fixedrange: true,
     },
     // dragmode: false,
     // zoommode: false,
   };
   let config = {
-    editable: true,
+    //editable: true,
     displaylogo: false,
     modeBarButtonsToRemove: ["toImage", "zoom2d", "lasso2d"],
+    responsive: true,
   };
 
   Plotly.newPlot(graphElement, [], layout, config);
