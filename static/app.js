cad20f51d4f008d549e187ffd06b88dd2097091d
mariamgamal70 <mariamgamal70@gmail.com>
2023-05-09 16:16:51 +0300

    Revert "reoved legend and the normalized slider"
    This reverts commit 6341ae97557c90fe98251185b4fded619ead0e1a.

--- static/app.js
+++ static/app.js
@@ -43,10 +43,10 @@ window.addEventListener("load", function () {
   Plotly.relayout(differenceGraph, { title: "Difference" });
 });
 
-// // update the sampling frequency according to normalized value slider
-// normalizedValueSlider.addEventListener("input", () => {
-//   updateSamplingRateNormalized();
-// });
+// update the sampling frequency according to normalized value slider
+normalizedValueSlider.addEventListener("input", () => {
+  updateSamplingRateNormalized();
+});
 
 // update the sampling frequency input according to actual value slider
 freqValueSlider.addEventListener("input", () => {
@@ -221,8 +221,6 @@ function createPlot(graphElement) {
     yaxis: {
       title: "Amplitude",
     },
-    showlegend: false,
-
   };
   Plotly.newPlot(graphElement, [], layout, {
     displaylogo: false,
@@ -272,9 +270,9 @@ function composeCosineSignal() {
   let amplitude = composerAmplitude.value;
   let wave = { x: [], y: [], name: "original signal" };
   // iterate over 1000 time durations to generate the signal of 1000 sample
-  for (let duration = 0; duration < 10000; duration++) {
+  for (let duration = 0; duration < 2000; duration++) {
     // calculate the time value for the current duration
-    let timeValue = duration / 1000;
+    let timeValue = duration / 500;
     // calculate the cosine value using the time and frequency values
     var value = amplitude * Math.cos(2 * Math.PI * frequency * timeValue);
     wave.x.push(timeValue);
